fe073d270e6a9abecadbf3bb5346c3d4e7bcaad4
INI Sathiamurthi <inis@inis-macbookpro.roam.corp.google.com>
2019-08-20 15:31:02 -0700

    format input to JSON

--- event_hub_client.py
+++ event_hub_client.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 class EventHubClient():
@@ -7,11 +8,24 @@ class EventHubClient():
         self.port = 8083
         self.headers = {'content-type': 'application/x-www-form-urlencoded'}
 
-    def publish_event(self, json_data):
-        
+    def publish_event(self, event_type, event_metadata):
+
+        json_data = self.convertToJson(event_type, event_metadata)
+
         publish_url = self.conn_url + ':' + str(self.port) + '/publish'
         
         data = {}
         data['jsonData'] = json_data
 
         print(requests.post(publish_url, data=data, headers=self.headers))
+
+    def convertToJson(self, event_type, event_metadata):
+        json_dict = {'name': event_type}
+        
+        for key in event_metadata.keys():
+            json_dict[key] = event_metadata[key]    
+
+        json_obj = json.dumps(json_dict)
+        
+        return str(json_obj)
+
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from event_hub_client import EventHubClient
 
 if __name__ == '__main__':
-    json_data = """{
-                    "name": "Website Visited",
-                    "orgId": "dzFyTqq4dT7YIai8mogz",
-                    "userId": "ABCDEFGHIJKLM",
-                    "sourceId": "ABCDEF",
-                    "timestamp": "2019-07-09T14:54:44.760Z"
-                }"""
 
+    event_type = "Website Visited"
+    event_metadata = {
+                        "orgId": "dzFyTqq4dT7YIai8mogz",
+                        "userId": "ABCDEFGHIJKLM",
+                        "sourceId": "ABCDEF",
+                        "timestamp": "2019-07-09T14:54:44.760Z"
+                    }
     e = EventHubClient()
-    e.publish_event(json_data)
+    e.publish_event(event_type, event_metadata)
+    # e.convertToJson(event_type, event_metadata)
